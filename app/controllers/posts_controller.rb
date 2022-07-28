17cc850065c28e9c560ef15c30f7a165faf131aa
Norman.Ablaza <namronazalba@gmail.com>
2022-07-28 17:31:31 +0800

    user can create, read, update and destroy the post without comments

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,76 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: :home
+  before_action :set_post, only: :show
+  before_action :set_own_post, only: [:edit, :update, :destroy]
+
+  def index
+    @posts = Post.includes(:user)
+    @domain = request.base_url
+    @posts = Post.all.includes(:user, :category).left_joins(:comments).group(:id).order("count(comments.id) desc")
+  end
+
+  def home
+    @posts = Post.includes(:user)
+    @posts = Post.all.includes(:user, :category).left_joins(:comments).group(:id).order("count(comments.id) desc")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def redirect
+    @maigsing_url = params[:url_short]
+    @post = Post.find_by(url_short: @maigsing_url)
+    redirect_to post_path(@post)
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def show; end
+
+  def destroy
+    if @post.comments.size == 0
+      @post.destroy
+      redirect_to posts_path
+    end
+  end
+
+  private
+
+  def set_own_post
+    @post = current_user.posts.find_by_id(params[:id])
+    if @post.nil?
+      flash[:alert] = 'this post not belongs to you or not exists'
+      redirect_to posts_path
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :address, :category_id, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,21 @@
+class Post < ApplicationRecord
+  validates_presence_of :title
+  validates_presence_of :content
+  validates_presence_of :address
+  validates_presence_of :generate_short_url
+
+  belongs_to :user
+  belongs_to :categories
+  has_many :comments
+
+  mount_uploader :image, ImageUploader
+
+  def generate_short_url
+    loop do
+      @surl = rand(1..9998).to_s.rjust(4,'0')
+      break unless Post.exists?(url_short: @surl)
+    end
+    self.url_short = @surl
+  end
+end
+
